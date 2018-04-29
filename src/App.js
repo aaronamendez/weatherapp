de0524e60655594f9f5dd20879e40f4b81655746
Aaron <aaronamendez1@gmail.com>
2018-04-28 21:06:45 -0400

    Setup components/ mac switch1/

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '20a3b07df621cd3bf33e6372d8b81f54';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-         <div className="container">
-             <div className="row">
-                 <div className="col days"> Sunday </div>
-                 <div className="col days"> Monday </div>
-                 <div className="col days"> Tuesday </div>
-                 <div className="col days"> Wednesday </div>
-                 <div className="col days"> Thursday </div>
-                 <div className="col days"> Friday </div>
-                 <div className="col days"> Saturday </div>
-             </div>
-         </div>
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
